7fbf76094c33172697f2d1cb8ba84750ca96f489
benjaminsinaiko <ben.jamin@outlook.com>
2018-04-25 09:45:57 -0400

    Pass tests for reverse-string

--- reverse-string/reverse-string.js
+++ reverse-string/reverse-string.js
@@ -1,5 +1,12 @@
 var ReverseString = function(string) {
-  //
+  if (string === "") {
+    return "";
+  } else {
+    return string
+      .split("")
+      .reverse()
+      .join("");
+  }
 };
 
 module.exports = ReverseString;
