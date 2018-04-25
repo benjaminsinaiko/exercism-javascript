665a6096714827b6612287d96acc61bdc9115007
benjaminsinaiko <ben.jamin@outlook.com>
2018-04-25 09:21:36 -0400

    Pass tests for Leap

--- leap/leap.js
+++ leap/leap.js
@@ -8,7 +8,19 @@ var Year = function(year) {
 };
 
 Year.prototype.isLeap = function() {
-  return false;
+  if (this.year % 4 === 0) {
+    if (this.year % 100 === 0) {
+      if (this.year % 400 === 0) {
+        return true;
+      } else {
+        return false;
+      }
+    } else {
+      return true;
+    }
+  } else {
+    return false;
+  }
 };
 
 module.exports = Year;
