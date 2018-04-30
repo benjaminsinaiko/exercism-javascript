6c0d73a486c159c753e8a12efc1671b06e5c20f8
benjaminsinaiko <ben.jamin@outlook.com>
2018-04-30 15:32:29 -0500

    Add simple-cipher.js file

---
+++ simple-cipher/simple-cipher.js
@@ -0,0 +1,12 @@
+var Cipher = function() {
+  var alpha = "abcdefghijklmnopqrstuvwxyz";
+  console.log(alpha);
+};
+
+Cipher.prototype.key = function() {
+  //
+};
+
+module.exports = Cipher;
+
+Cipher();
